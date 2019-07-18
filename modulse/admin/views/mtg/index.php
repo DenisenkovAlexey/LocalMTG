2afe5d8b5f8b0e9027289884b4a5d7682d06fc15
Alokrion <Spoc2@yandex.ru>
2019-07-18 19:33:35 +0500

    add parser class
    Signed-off-by: Alokrion <Spoc2@yandex.ru>

--- modulse/admin/views/mtg/index.php
+++ modulse/admin/views/mtg/index.php
@@ -1,6 +1,6 @@
 <?php
 
-?>
+use yii\widgets\Menu; ?>
 <div class="admin-default-index">
     <h1><?= $this->context->action->uniqueId ?></h1>
     <p>
@@ -11,10 +11,10 @@
     <p>
         <?php
         $menuItems[] =[ ['label' => 'ссылка 1', 'url' => ['#']], ['label' => 'ссылка 2', 'url' => ['#']]];
-        echo \yii\widgets\Menu::widget(
+        echo Menu::widget(
         [
                 'items' => [['label' => 'ссылка 1', 'url' => ['#']], ['label' => 'ссылка 2', 'url' => ['#']]],
             ]
         )
+
         ?>
-    </p>
